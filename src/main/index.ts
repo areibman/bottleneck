196b636a99e6206a0e31e5ee0ff94091b8295cfa
Alex Reibman <meta.alex.r@gmail.com>
2025-10-03 19:39:44 -0700

    overzealous * removal

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
 import { app, BrowserWindow, ipcMain, shell, Menu, dialog } from "electron";
 import path from "path";
-import * as dotenv from "dotenv";
-dotenv.config();
+import { config } from "dotenv";
+config();
 
 import { GitHubAuth } from "./auth";
 import { GitOperations } from "./git";
